bf6a27518d94c393192ed00668b6e0b9ceb9c985
Dave Baskin <dbaskin@wintellect.com>
2015-03-04 23:47:32 -0500

    Updates.

---
+++ graph/src/startup.js
@@ -0,0 +1,18 @@
+
+Office.initialize = function () {
+    console.log(">>> Office.initialize()");
+
+    Office.context.document.bindings.addFromPromptAsync('matrix', function (asyncResult) {
+        if (asyncResult.status == Office.AsyncResultStatus.Succeeded) {
+            console.log(asyncResult);
+
+            asyncResult.value.getDataAsync(function(data) {
+                console.log(data);
+
+            });
+        }
+    });
+
+
+};
+
